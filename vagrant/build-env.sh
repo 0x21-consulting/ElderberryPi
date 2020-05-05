de40dacca14ea86c49657a36c1315a634ab5bb64
Micah Henning <micah@0x21.consulting>
2020-05-05 15:02:15 -0500

    Hide some log details. Start zymbit config.

--- vagrant/build-env.sh
+++ vagrant/build-env.sh
@@ -7,12 +7,14 @@
 PACKER_VERSION="1.5.6"
 
 # Update the system
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y autoremove
+echo "Updating Vagrant box..."
+sudo apt-get update &>/dev/null
+sudo apt-get -y upgrade &>/dev/null
+sudo apt-get -y autoremove &>/dev/null
 sudo apt-get clean
 
 # Install required packages
+echo "Installing build tools..."
 sudo apt-get install -y \
   qemu-user-static \
   ca-certificates \
@@ -22,21 +24,23 @@ sudo apt-get install -y \
   libarchive-tools \
   git \
   golang-go \
-  unzip
+  unzip &>/dev/null
 sudo apt-get clean
 
 # Download and install packer
 command -v packer >/dev/null 2>&1 || {
-  wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip -O /tmp/packer.zip && \
-  sudo unzip /tmp/packer.zip -d /bin && \
+  echo "Installing packer..."
+  wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip -O /tmp/packer.zip &>/dev/null
+  sudo unzip /tmp/packer.zip -d /bin &>/dev/null
   rm /tmp/packer.zip
 }
 
 # Download and build packer-builder-arm
 if [ ! -f /vagrant/packer-builder-arm ]; then
-  git clone https://github.com/mkaczanowski/packer-builder-arm
+  echo "Installing packer-builder-arm provisioner..."
+  git clone https://github.com/mkaczanowski/packer-builder-arm &>/dev/null
   cd packer-builder-arm
-  go mod download
-  go build
+  go mod download &>/dev/null
+  go build &>/dev/null
   cp packer-builder-arm /vagrant/
 fi
